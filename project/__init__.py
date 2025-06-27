b7a7aa813a1bf5dff5958010ad0f8475829f2447
Antoine Rozo <antoine.rozo@getalma.eu>
2025-06-27 10:48:35 +0200

    Add failures

---
+++ project/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+
+def main( arg: int = None ) -> None:
+    return 0
--- tests/test_empty.py
+++ tests/test_empty.py
@@ -1,2 +1,7 @@
 def test_empty():
     assert True
+
+
+def test_main():
+    from project import main
+    assert main() is None
